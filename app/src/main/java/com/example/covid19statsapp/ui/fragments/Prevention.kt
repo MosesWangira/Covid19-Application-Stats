3f58fc985e192de7f562896a8b160d132c1790c3
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-11-29 12:29:04 +0300

    add all fragments

---
+++ app/src/main/java/com/example/covid19statsapp/ui/fragments/Prevention.kt
@@ -0,0 +1,19 @@
+package com.example.covid19statsapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.covid19statsapp.R
+
+class Prevention : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_prevention, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/covid19statsapp/ui/fragments/Summary.kt
@@ -0,0 +1,20 @@
+package com.example.covid19statsapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.covid19statsapp.R
+
+class Summary : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_summary, container, false)
+    }
+
+}
