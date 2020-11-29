6308b9e0caa92f5d06f33f00531e29951c98be2f
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2020-11-29 11:15:26 +0300

    splash screen activity

---
+++ app/src/main/java/com/example/covid19statsapp/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.covid19statsapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covid19statsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/example/covid19statsapp/ui/activities/SplashScreen.kt
+++ app/src/main/java/com/example/covid19statsapp/ui/activities/SplashScreen.kt
@@ -1,12 +1,12 @@
-package com.example.covid19statsapp.ui
+package com.example.covid19statsapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.covid19statsapp.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
